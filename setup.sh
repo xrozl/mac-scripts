4fa8c81c4d7d07b1b73fb1db318263789c594890
yuu <92621736+xrozl@users.noreply.github.com>
2022-07-31 03:08:35 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+
+
+brew install wget
+brew install git 
+brew install python
+brew install neovim
+brew install --cask github
+brew install --cask visual-studio-code
